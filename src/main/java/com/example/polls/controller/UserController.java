7a37fdb4ecbd3956f79814c76166c843f06ac362
nadedai <53480408+nadedai@users.noreply.github.com>
2020-08-05 22:40:07 +0800

    add

--- src/main/java/com/example/polls/controller/UserController.java
+++ src/main/java/com/example/polls/controller/UserController.java
@@ -58,9 +58,8 @@ public class UserController {
         return userSummaries;
     }
 
-
     @GetMapping("/users")
-    @PreAuthorize("hasRole('ADMIN') or hasRole('ADMIN')")
+    @PreAuthorize("hasRole('ADMIN')")
     public PagedResponse<UserResponse> getUsers(@CurrentUser UserPrincipal currentUser,
                                       @RequestParam(value = "page", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER) int page,
                                       @RequestParam(value = "size", defaultValue = AppConstants.DEFAULT_PAGE_SIZE) int size) {
